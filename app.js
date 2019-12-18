20986eabfeaf4e0986fb7916a1d29bde2574cc00
dasen <869490494@qq.com>
2019-12-18 22:29:45 +0800

    middle ware

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ const skuRouter = require('./router/sku.router')
 
 //中间件
 function middle_ware(req, res, next) {
-	let {age} = req.query;
-	if (age < 20) {
-		res.json({
-			code: 200,
-			info: "您年龄太小"
-		})
+    let {age} = req.query;
+    if (age < 20) {
+        res.json({
+            code: 200,
+            info: "您年龄太小"
+        })
 
-	} else {
-		next()
-	}
+    } else {
+        next()
+    }
 }
 
 //app级别中间件
@@ -28,24 +28,25 @@ app.all('*', middle_ware)
 
 //第三方中间件
 app.use(cookieParser());
-app.get('/member/', function (req, res, next) {
-	const {age} = req.query
-	console.log(req.cookies); // 解析过的object的形式--{ k1: 'v1', k2: 'v2' }。引入了cookie-parser方可使用
-	console.log(req.headers.cookie);// string的形式--k1=v1; k2=v2。express原生可以用
-	res.cookie( age); // 设置cookie,express原生可以用
-	// next()
-    res.send('cookie example');
 
-})
 
 //应用路由
 app.use('/member', membeRouter)
 app.use('/sku', skuRouter)
+app.get('/middle', function (req, res, next) {
+    const {age} = req.query
+    console.log(age);
+    console.log(req.cookies); // 解析过的object的形式--{ k1: 'v1', k2: 'v2' }。引入了cookie-parser方可使用
+    console.log(req.headers.cookie);// string的形式--k1=v1; k2=v2。express原生可以用
 
+    // next()
+    res.send('cookie example');
+
+})
 
 //端口监听
 app.listen(3000, () => {
-	console.log('3000 端口启动成功！');
+    console.log('3000 端口启动成功！');
 })
 
 
--- router/member.touter.js
+++ router/member.touter.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const router = express.Router()
-
+var cookieParser = require('cookie-parser');
 const database = require('../mysql/database')//链接数据库
+router.use(cookieParser());
+router.get('/list/:name', (req, res) => {
+    let {age} = req.query;
+    let {name} = req.params;
+    console.log(name);
 
-router.get('/list', (req, res) => {
-	let {age} = req.params;
-	database('select * from login', [], (err, data) => {
-		if (err) {
-			res.json({
-				code: 101,
-				info: '获取失败',
-				data: err
-			})
-		}
-		if (data) {
+    res.cookie('age', age); // 设置cookie,express原生可以用
+    res.cookie('name', name); // 设置cookie,express原生可以用
+    database('select * from login', [], (err, data) => {
+        if (err) {
+            res.json({
+                code: 101,
+                info: '获取失败',
+                data: err
+            })
+        }
+        if (data) {
 
-			res.json({
-				code: 200,
-				info: '获取成功',
-				data: data
-			})
-		}
-	})
+            res.json({
+                code: 200,
+                info: '获取成功',
+                data: data
+            })
+        }
+    })
 })
 
 
